a33f7149fe61077c1c0d8d4907dda4f70db81d47
Anoop Sownderraj <asownder95@gmail.com>
2018-12-27 11:11:58 -0800

    Implemented Search Recipe intent handler

--- fulfillment/handlers.js
+++ fulfillment/handlers.js
@@ -19,10 +19,33 @@ const askInitialIntent = (conv, params, permissionGranted) => {
   } else {
     conv.user.storage.name = conv.user.name;
     conv.ask(`Thanks ${conv.user.storage.name.given}! What would you like to do today?`);
+    conv.ask(new Suggestions('Search Recipes'));
   }
+};
+
+const confirmRecipeQuery = (conv, { food, dietaryRestriction }) => {
+  const intolerances = new Set(['dairy', 'egg', 'gluten', 'peanut', 'sesame', 'seafood', 'shellfish', 'soy', 'tree nuts', 'wheat']);
+  const restrictions = {
+    diet: [],
+    intolerances: [],
+  };
+  dietaryRestriction.forEach((item) => {
+    if (item === 'vegetarian' || item === 'vegan') {
+      restrictions.diet.push(item);
+    } else if (intolerances.has(item)) {
+      restrictions.intolerances.push(item);
+    }
+  });
+  conv.data.restrictions = restrictions;
+  restrictions.diet = restrictions.diet.join(', ');
+  restrictions.intolerances = restrictions.intolerances.join(', ');
+  const responseDietSection = restrictions.diet.length ? `${restrictions.diet} ${food}` : `${food}`;
+  const responseIntolerancesSection = restrictions.intolerances.length ? `without ${restrictions.intolerances} right?` : 'right?';
+  conv.ask(`Just to confirm, you wanted to look for ${responseDietSection} recipes ${responseIntolerancesSection}`);
 };
 
 module.exports = {
   askNamePermission,
   askInitialIntent,
+  confirmRecipeQuery,
 };
--- server/assistant.js
+++ server/assistant.js
@@ -1,10 +1,11 @@
 const { dialogflow } = require('actions-on-google');
 
 const assistant = dialogflow({ debug: true });
-const { askNamePermission, askInitialIntent } = require('../fulfillment/handlers.js');
+const { askNamePermission, askInitialIntent, confirmRecipeQuery } = require('../fulfillment/handlers.js');
 
 assistant.intent('Default Welcome Intent', askNamePermission);
 assistant.intent('actions_intent_PERMISSION', askInitialIntent);
+assistant.intent('Search Recipes', confirmRecipeQuery);
 
 assistant.catch((conv, err) => {
   console.error(err);
