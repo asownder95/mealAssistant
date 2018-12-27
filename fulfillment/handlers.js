ebae80d13a98b952375b1c0946332f387cf603f4
Anoop Sownderraj <asownder95@gmail.com>
2018-12-27 11:29:27 -0800

    Implemented Query Recipes intent handler

--- fulfillment/handlers.js
+++ fulfillment/handlers.js
@@ -1,4 +1,6 @@
 const { Permission, Suggestions } = require('actions-on-google');
+const request = require('request-promise');
+const rapidAPIKey = require('../config.js');
 
 const askNamePermission = (conv) => {
   const name = conv.user.storage.name;
@@ -44,8 +46,25 @@ const confirmRecipeQuery = (conv, { food, dietaryRestriction }) => {
   conv.ask(`Just to confirm, you wanted to look for ${responseDietSection} recipes ${responseIntolerancesSection}`);
 };
 
+
+const requestRecipes = (conv) => {
+  const queryParamsFood = conv.contexts.input["searchrecipes-followup"].parameters.food ? `query=${conv.contexts.input["searchrecipes-followup"].parameters.food.replace(' ', '+')}` : '';
+  const queryParamsDiet = conv.data.restrictions.diet ? `diet=${conv.data.restrictions.diet.replace(', ', '%2C')}&` : '';
+  const queryParamsIntolerances = conv.data.restrictions.intolerances ? `intolerances=${conv.data.restrictions.intolerances.replace(', ', '%2C')}&` : '';
+  request({
+    uri: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?${queryParamsDiet}${queryParamsIntolerances}number=1&offset=0&${queryParamsFood}`,
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': rapidAPIKey,
+    },
+  }).then(res => console.log(`This is the response: ${JSON.stringify(res)}`))
+    .catch(err => console.error(err));
+};
+
+
 module.exports = {
   askNamePermission,
   askInitialIntent,
   confirmRecipeQuery,
+  requestRecipes,
 };
--- server/assistant.js
+++ server/assistant.js
@@ -1,11 +1,12 @@
 const { dialogflow } = require('actions-on-google');
 
 const assistant = dialogflow({ debug: true });
-const { askNamePermission, askInitialIntent, confirmRecipeQuery } = require('../fulfillment/handlers.js');
+const { askNamePermission, askInitialIntent, confirmRecipeQuery, requestRecipes } = require('../fulfillment/handlers.js');
 
 assistant.intent('Default Welcome Intent', askNamePermission);
 assistant.intent('actions_intent_PERMISSION', askInitialIntent);
 assistant.intent('Search Recipes', confirmRecipeQuery);
+assistant.intent('Query Recipes', requestRecipes);
 
 assistant.catch((conv, err) => {
   console.error(err);
